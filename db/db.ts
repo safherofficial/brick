ea53f168aced41d1974d6e249afa8d0eff382143
safherofficial <safherofficial@gmail.com>
2026-09-10 19:44:07 +0200

    Delete db/db.ts

--- db/db.ts
+++
@@ -1,7 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-
-export function sql() {
-  const url = process.env.POSTGRES_URL;
-  if (!url) throw new Error("Missing POSTGRES_URL");
-  return neon(url);
-}
